8a4cd3d3bc9bb7f16cdc27bf5c275bceccc50af9
sakuragi <wustgscoder@163.com>
2017-11-20 09:59:52 +0800

    conflict solved

--- DaaaQing/Classes/Home/Controllers/GSHomeViewController.m
+++ DaaaQing/Classes/Home/Controllers/GSHomeViewController.m
@@ -24,42 +24,9 @@
     [self showBirthdayView];
     [self setupUI];
     
-   
 
-
-//    self.title = @"DaQqqqqq";
-
-    
-//    self.view.backgroundColor = [UIColor randomColor];
-
-    [[GSAudioTool sharedAudioTool] playBirthSong];
-//    [self showBirthdayView];
-    
-
-    
-//    UIButton *testBtn = [[UIButton alloc] init];
-//    [testBtn setTitle:@"test" forState:UIControlStateNormal];
-//    [testBtn setBackgroundColor:[UIColor honeydew]];
-//    [testBtn setTitleColor:[UIColor seaGreen] forState:UIControlStateNormal];
-
-//    
-//    [testBtn addTarget:self action:@selector(showBirthdayView) forControlEvents:UIControlEventTouchUpInside];
-//    testBtn.frame = CGRectMake(20, 70, 100, 40);
-//    
-//    [self.view addSubview:testBtn];
-    
-//    
-//    UIImageView *imageView = [[UIImageView alloc] initWithFrame:CGRectMake(20, 200, 300, 300)];
-//    
-//    UIImage *image = [[GSTool sharedTool] qrCoderWithContent:@"201709250047$421127001008GB00665F00010089$鄂(2017)黄梅不动产权第0009713号$F70034CB5FF56F63C3A8D9C1"];
-//    
-//    imageView.image = image;
-//    
-//    UIImageWriteToSavedPhotosAlbum(image, nil, nil, nil);
-//    
-//    [self.view addSubview:imageView];
-//
-    
+//    [[GSAudioTool sharedAudioTool] playBirthSong];
+  
 }
 
 
@@ -179,71 +146,14 @@
         [text appendAttributedString:[self padding]];
     }
     
+    return text;
+
     
-=======
-//
-//    [testBtn addTarget:self action:@selector(showBirthdayView) forControlEvents:UIControlEventTouchUpInside];
-//    testBtn.frame = CGRectMake(20, 70, 100, 40);
-//
-//    [self.view addSubview:testBtn];
-//
-////
-//    UIImageView *imageView = [[UIImageView alloc] initWithFrame:CGRectMake(20, 200, 300, 300)];
-//
-//    UIImage *image = [[GSTool sharedTool] qrCoderWithContent:@"201709250047$421127001008GB00665F00010089$鄂(2017)黄梅不动产权第0009713号$F70034CB5FF56F63C3A8D9C1"];
-//
-//    imageView.image = image;
-//
-//    UIImageWriteToSavedPhotosAlbum(image, nil, nil, nil);
-//
-//    [self.view addSubview:imageView];
-//
-    
-}
-- (void)shakeToChangeEnvironmentEnable:(BOOL)enable
-{
-    //设置摇一摇功能
-    [UIApplication sharedApplication].applicationSupportsShakeToEdit = enable;
-    
-    //并让自己成为第一响应者
-    [self becomeFirstResponder];
 }
 
-//开始摇动
-- (void)motionBegan:(UIEventSubtype)motion withEvent:(UIEvent *)event
-{
-    if (_aletShow) return; //弹窗已经弹出
-    _aletShow = YES;
-    NSNumber *environment = [[NSUserDefaults standardUserDefaults] objectForKey:@"userEnvironment"];
-    NSArray *environmentArray = @[@"测试环境",@"开发环境",@"灰度环境",@"正式环境"];
-    NSString *currentEnvironment = environmentArray[environment.integerValue];
-    UIAlertController *alertVC = [UIAlertController alertControllerWithTitle:[NSString stringWithFormat:@"当前环境:%@", currentEnvironment] message:@"切换不同环境需要重新登录哦😀" preferredStyle:UIAlertControllerStyleAlert];
-    for (int i = 0; i < environmentArray.count; i++) {
-        NSString *e = environmentArray[i];
-        if (i == environment.integerValue) {
-            e = [e stringByAppendingString:@"(当前)"];
-        }
-        UIAlertAction *action = [UIAlertAction actionWithTitle:e style:UIAlertActionStyleDefault handler:^(UIAlertAction * _Nonnull action) {
-            _aletShow = NO;
-            [[NSUserDefaults standardUserDefaults] setObject:[NSNumber numberWithInteger:i] forKey:@"userEnvironment"];
-            
-            // to-do : 切换环境
-        }];
-        [alertVC addAction:action];
-    }
-    
-    UIAlertAction *cancel = [UIAlertAction actionWithTitle:@"取消" style:UIAlertActionStyleCancel handler:^(UIAlertAction * _Nonnull action) {
-        _aletShow = NO;
-    }];
-    
-    [alertVC addAction:cancel];
-    
-    [self presentViewController:alertVC animated:YES completion:nil];
->>>>>>> d97cd67b2c774df5e64075a57865d0f4e681dcc5
-    
-    return text;
-}
-<<<<<<< HEAD
+
+
+
 
 - (NSAttributedString *)padding {
     NSMutableAttributedString *pad = [[NSMutableAttributedString alloc] initWithString:@"\n\n"];
@@ -263,17 +173,6 @@
 
 - (void)segueToDiary
 {
-=======
-- (void)showBirthdayView
-{
-    //    GSBirthdayView *view = [[GSBirthdayView alloc] init];
-    //    [view show];
-    
-    //    GSFireViewController *vc = [[GSFireViewController alloc] init];
-    //    [self.navigationController pushViewController:vc animated:YES];
-    //
-    
->>>>>>> d97cd67b2c774df5e64075a57865d0f4e681dcc5
     GSDiaryController *vc = [[GSDiaryController alloc] init];
     [self.navigationController pushViewController:vc animated:YES];
 }
